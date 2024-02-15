509352f7b863f5afbc7746a0203d36c9222418fd
Ana Popova <ana.popova83@gmail.com>
2024-02-15 14:20:56 +0200

    feat(funk): navbar hide home button

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,67 +1,3 @@
-// import React, { useState } from 'react';
-// import { Outlet, useNavigate } from 'react-router-dom';
-// import {
-//   Bar,
-//   BurgerMenu,
-//   NavContainer,
-//   NavLinks,
-//   StyledNavLink,
-//   ResponsiveStyles,
-//   Logo,
-//   RootStyles
-// } from './NavBar.style';
-// import { imageSrcPath } from './NavBar.static';
-
-// const NavBar: React.FC = () => {
-//   const [open, setOpen] = useState(false);
-//   const navigate = useNavigate();
-
-//   const handleLogout = () => {
-//     localStorage.removeItem('token');
-//     navigate('/logout');
-//   };
-
-//   const toggleNav = () => {
-//     setOpen((prevOpen) => !prevOpen);
-//   };
-
-//   return (
-//     <ResponsiveStyles>
-//       <RootStyles>
-//         <header>
-//           <NavContainer>
-//             <NavLinks>
-//               <Logo>
-//                 <img src={imageSrcPath} width="30" height="30" alt="box" />
-//               </Logo>
-//               <BurgerMenu onClick={toggleNav}>
-//                 <Bar>{open ? 'Close' : 'Open'}</Bar>
-//                <div>line 1</div>
-//                <div>line 2</div>
-//                <div>line 3</div>
-//               </BurgerMenu>
-//               <StyledNavLink to="/" end>
-//                 Home
-//               </StyledNavLink>
-//               <StyledNavLink to="/auth">LogIn / SignUp</StyledNavLink>
-//               <StyledNavLink to="/client">Client</StyledNavLink>
-//               <StyledNavLink to="/product">Product</StyledNavLink>
-//               <StyledNavLink to="/order">Order</StyledNavLink>
-//               <StyledNavLink to="/warehouse">Warehouse</StyledNavLink>
-//             </NavLinks>
-//             <StyledNavLink to="/logout" onClick={handleLogout} style={{ display: 'none' }}>Logout</StyledNavLink>
-//           </NavContainer>
-//         </header>
-//         <main>
-//           <Outlet />
-//         </main>
-//       </RootStyles>
-//     </ResponsiveStyles>
-//   );
-// };
-
-// export default NavBar;
-
 import React, { useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import {
@@ -104,12 +40,12 @@ const NavBar: React.FC = () => {
                 <Bar />
                 <Bar />
               </BurgerMenu>
-              <StyledNavLink to="/" end>
-                Home
-              </StyledNavLink>
               <StyledNavLink to="/auth">LogIn / SignUp</StyledNavLink>
               {localStorage.getItem("token") && (
                 <>
+                  <StyledNavLink to="/" end>
+                    Home
+                  </StyledNavLink>
                   <StyledNavLink to="/client">Client</StyledNavLink>
                   <StyledNavLink to="/product">Product</StyledNavLink>
                   <StyledNavLink to="/order">Order</StyledNavLink>
