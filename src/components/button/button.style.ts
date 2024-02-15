38d7ddf8a8da4327e4a00bf390284db32cd40ae4
Ana Popova <ana.popova83@gmail.com>
2024-02-15 14:18:22 +0200

    feat(funk): button font-size adjust

--- src/components/button/button.style.ts
+++ src/components/button/button.style.ts
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 export const Button = styled.button`
   margin: 10px;
   padding: 8px 12px;
-  font-size: 12px;
+  font-size: 14px;
   background-color: #32465c;
   color: #fff;
   border: none;
@@ -15,14 +15,14 @@ export const Button = styled.button`
   }
 
   @media only screen and (max-width: 768px) {
-    font-size: 8px;
+    font-size: 12px;
     padding: 6px 10px;
   }`;
 
 export const RedButton = styled.button`
 margin: 10px;
 padding: 8px 12px;
-font-size: 12px;
+font-size: 14px;
 background-color: brown;
 color: whitesmoke;
 border: none;
@@ -34,6 +34,6 @@ cursor: pointer;
 }
 
 @media only screen and (max-width: 768px) {
-  font-size: 8px;
+  font-size: 12px;
   padding: 6px 10px;
 }`;
